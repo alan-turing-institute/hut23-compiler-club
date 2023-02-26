9493f4f1e1446fddbff43c84dea35f29cf4b7bee
James Geddes <james@vouga.home>
2023-02-26 21:34:27 +0000

    Makes

--- expt0/run.s
+++ expt0/run.s
@@ -3,165 +3,102 @@
 
   
   .syntax unified
+  .thumb
+  .cpu cortex-m0
   .arch armv6-m                 /* Overridden by -mcpu, I suspect */
-
-  .text
-__reset:
-  b reset
 
   @@@ The following is adapted from Nordic's gcc_startup_nrf51.S
 
+  /* The exception vector table consists of:
+  - 16 words, defined for the M0 arhictecture
+  - 32 words, defining external interrupts
+
+See: Cortex M0 Generic Userguide, 2.3.2, as well as table 2-1
+  */ 
+
   .section .vectors
-  .globl __isr_vecto  r
-__isr_vector_table: 
-    .long   __StackTop                  /* Top of Stack */
-    .long   Reset_Handler
-    .long   NMI_Handler
-    .long   HardFault_Handler
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   SVC_Handler
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   PendSV_Handler
-    .long   SysTick_Handler
+  .global __vector_table
 
-  /* External Interrupts */
-    .long   POWER_CLOCK_IRQHandler
-    .long   RADIO_IRQHandler
-    .long   UART0_IRQHandler
-    .long   SPI0_TWI0_IRQHandler
-    .long   SPI1_TWI1_IRQHandler
-    .long   0                           /*Reserved */
-    .long   GPIOTE_IRQHandler
-    .long   ADC_IRQHandler
-    .long   TIMER0_IRQHandler
-    .long   TIMER1_IRQHandler
-    .long   TIMER2_IRQHandler
-    .long   RTC0_IRQHandler
-    .long   TEMP_IRQHandler
-    .long   RNG_IRQHandler
-    .long   ECB_IRQHandler
-    .long   CCM_AAR_IRQHandler
-    .long   WDT_IRQHandler
-    .long   RTC1_IRQHandler
-    .long   QDEC_IRQHandler
-    .long   LPCOMP_IRQHandler
-    .long   SWI0_IRQHandler
-    .long   SWI1_IRQHandler
-    .long   SWI2_IRQHandler
-    .long   SWI3_IRQHandler
-    .long   SWI4_IRQHandler
-    .long   SWI5_IRQHandler
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
-    .long   0                           /*Reserved */
+__vector_table:                 @   No. IRQ Pri
+  /* Exceptions */              @   --- --- ---
+    .word   __stack             @     -   -   -       
+    .word   __reset             @     1   -  -3 
+    .word   NMI_Handler         @     2 -14  -2   
+    .word   HardFault_Handler   @     3 -13  -1
+    .word   0 /* Reserved */
+    .word   0 /* Reserved */                                 
+    .word   0 /* Reserved */
+    .word   0 /* Reserved */
+    .word   0 /* Reserved */
+    .word   0 /* Reserved */
+    .word   0 /* Reserved */
+    .word   SVC_Handler         @    11  -5 
+    .word   0 /* Reserved */
+    .word   0 /* Reserved */
+    .word   PendSV_Handler      @    14  -2
+    .word   SysTick_Handler     @    15  -1
 
-    .size __isr_vector, . - __isr_vector
+  /* External Interrupts */     
+    .word   POWER_CLOCK_IRQHandler  
+    .word   RADIO_IRQHandler
+    .word   UART0_IRQHandler
+    .word   SPI0_TWI0_IRQHandler
+    .word   SPI1_TWI1_IRQHandler
+    .word   0                           /*Reserved */
+    .word   GPIOTE_IRQHandler
+    .word   ADC_IRQHandler
+    .word   TIMER0_IRQHandler
+    .word   TIMER1_IRQHandler
+    .word   TIMER2_IRQHandler
+    .word   RTC0_IRQHandler
+    .word   TEMP_IRQHandler
+    .word   RNG_IRQHandler
+    .word   ECB_IRQHandler
+    .word   CCM_AAR_IRQHandler
+    .word   WDT_IRQHandler
+    .word   RTC1_IRQHandler
+    .word   QDEC_IRQHandler
+    .word   LPCOMP_IRQHandler
+    .word   SWI0_IRQHandler
+    .word   SWI1_IRQHandler
+    .word   SWI2_IRQHandler
+    .word   SWI3_IRQHandler
+    .word   SWI4_IRQHandler
+    .word   SWI5_IRQHandler
+    .word   0                           /*Reserved */
+    .word   0                           /*Reserved */
+    .word   0                           /*Reserved */
+    .word   0                           /*Reserved */
+    .word   0                           /*Reserved */
+    .word   0                           /*Reserved */
 
+  /*  
+  Gnu as, 7.89: This directive sets the size associated with a
+  symbol name. The size in bytes is computed from expression which can
+  make use of label arithmetic. This directive is typically used to
+  set the size of function symbols.
+  */
+  /* I wonder if this is help the C compiler address the vector table as an array?
+  
+  .size __isr_vector, . - __isr_vector
+
+  /*  
+  
 /* Reset Handler */
 
-    .equ    NRF_POWER_RAMON_ADDRESS,             0x40000524
-    .equ    NRF_POWER_RAMONB_ADDRESS,            0x40000554
-    .equ    NRF_POWER_RAMONx_RAMxON_ONMODE_Msk,  0x3
+  .text
 
-    .text
-    .thumb
-    .thumb_func
-    .align 1
-    .globl Reset_Handler
-    .type Reset_Handler, %function
-Reset_Handler:
+  .thumb
+  .thumb_func
+  .align 1
+  .global __reset
+  .type __reset, %function
+__reset:
 
-    MOVS    R1, #NRF_POWER_RAMONx_RAMxON_ONMODE_Msk
-    
-    LDR     R0, =NRF_POWER_RAMON_ADDRESS
-    LDR     R2, [R0]
-    ORRS    R2, R1
-    STR     R2, [R0]
+  nop
+  wfe
 
-    LDR     R0, =NRF_POWER_RAMONB_ADDRESS
-    LDR     R2, [R0]
-    ORRS    R2, R1
-    STR     R2, [R0]
-
-/* Loop to copy data from read only memory to RAM.
- * The ranges of copy from/to are specified by following symbols:
- *      __etext: LMA of start of the section to copy from. Usually end of text
- *      __data_start__: VMA of start of the section to copy to.
- *      __bss_start__: VMA of end of the section to copy to. Normally __data_end__ is used, but by using __bss_start__
- *                    the user can add their own initialized data section before BSS section with the INSERT AFTER command.
- *
- * All addresses must be aligned to 4 bytes boundary.
- */
-#ifndef __STARTUP_SKIP_ETEXT
-    ldr r1, =__etext
-    ldr r2, =__data_start__
-    ldr r3, =__bss_start__
-
-    subs r3, r3, r2
-    ble .L_loop1_done
-
-.L_loop1:
-    subs r3, r3, #4
-    ldr r0, [r1,r3]
-    str r0, [r2,r3]
-    bgt .L_loop1
-
-.L_loop1_done:
-#endif
-
-/* This part of work usually is done in C library startup code. Otherwise,
- * define __STARTUP_CLEAR_BSS to enable it in this startup. This section
- * clears the RAM where BSS data is located.
- *
- * The BSS section is specified by following symbols
- *    __bss_start__: start of the BSS section.
- *    __bss_end__: end of the BSS section.
- *
- * All addresses must be aligned to 4 bytes boundary.
- */
-#ifdef __STARTUP_CLEAR_BSS
-    ldr r1, =__bss_start__
-    ldr r2, =__bss_end__
-
-    movs r0, 0
-
-    subs r2, r2, r1
-    ble .L_loop3_done
-
-.L_loop3:
-    subs r2, r2, #4
-    str r0, [r1, r2]
-    bgt .L_loop3
-
-.L_loop3_done:
-#endif /* __STARTUP_CLEAR_BSS */
-
-/* Execute SystemInit function. */
-    bl SystemInit
-
-/* Call _start function provided by libraries.
- * If those libraries are not accessible, define __START as your entry point.
- */
-#ifndef __START
-#define __START _start
-#endif
-    bl __START
-
-    .pool
-    .size   Reset_Handler,.-Reset_Handler
-
-    .section ".text"
-
+  .size   __reset, . - __reset
 
 /* Dummy Exception Handlers (infinite loops which can be modified) */
 
